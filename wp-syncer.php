35746090c41e7a766655e3e4e48ac87c990fd3f8
Joro Stoyanov <stoyanov.gs@gmail.com>
2015-10-29 23:13:56 +0200

    start working on the main plugin singleton class

--- wp-syncer.php
+++ wp-syncer.php
@@ -3,3 +3,42 @@
  * Plugin Name: WP Syncer
  * Description: Allows Syncing content between multiple databases
  */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Main plugin class
+ */
+final class WP_Syncer() {
+	/**
+	 * Constructor
+	 *
+	 * @access private
+	 */
+	private function __construct() {
+
+	}
+
+	/**
+	 * Returns the class main instance
+	 * 
+	 * @static
+	 * @access public
+	 * 
+	 * @return WP_Syncer
+	 */
+	static public function get_instance() {
+		if ( self::$instance === null ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+}
+
+function wp_syncer() {
+	return WP_Syncer::get_instance();
+}
+
+// initialization
+wp_syncer();
